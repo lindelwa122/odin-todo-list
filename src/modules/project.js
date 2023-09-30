9ebd423df56a7d317ebf55b0c11bd360465f4a71
Lindelwa <lindelwa.codes@gmail.com>
2023-09-30 15:04:26 +0200

    Add project

---
+++ src/modules/project.js
@@ -0,0 +1,41 @@
+import { nanoid } from "nanoid";
+
+/**
+ * Represents a group of todos
+ * @param {string} title - The title of the project
+ * @param {string} descr - The description of the project
+ * @returns public methods of project
+ */
+const project = (title, descr) => {
+  const _id = nanoid();
+  const _todos = [];
+
+  const getAll = () => _todos;
+  const getID = () => _id;
+  const getTitle = () => title;
+  const getDescr = () => descr;
+
+  /**
+   * Adds a new todo
+   * @param {{}} todo  - A todo instance
+   */
+  const addTodo = (todo) => {
+    _todos.push(todo);
+  };
+
+  /**
+   * Removes a todo and if successfuly returns true; otherwise false.
+   * @param {string} id
+   * @returns
+   */
+  const removeTodo = (id) => {
+    const index = _todos.findIndex((todo) => todo.id === id);
+    if (index === -1) return false;
+    _todos.splice(index, 1);
+    return true;
+  };
+
+  return { addTodo, getAll, getDescr, getID, getTitle, removeTodo };
+};
+
+export default project;
