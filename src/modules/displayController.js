e37db77a6a90f2c591a104253263df6b169d880e
Lindelwa <lindelwa.codes@gmail.com>
2023-10-14 09:17:17 +0200

    Add functionality to add a new project

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -9,6 +9,55 @@ const displayController = () => {
     });
   };
 
+  const _renderProjects = () => {
+    const projects = userInterfaceAPI.getAllProjects();
+
+    const projectElement = (id, name, description) => {
+      const container = document.createElement('div');
+      container.dataset.id = id;
+      container.title = description;
+
+      const icon = document.createElement('i');
+      icon.classList = ['bi'];
+
+      const text = document.createElement('span');
+      text.textContent = name;
+
+      container.append(icon, text);
+
+      return container;
+    }
+
+    const emptyContainer = () => {
+      const container = document.querySelector('#project-list');
+
+      // Convert HTML Collection to an Array instance
+      // This allow us to remove a child from the DOM without the array updating
+      const children = [];
+      for (const child of container.children) children.push(child);
+
+      for (const child of children) {
+        if (!child.classList.contains('heading')) {
+          child.remove();
+        }
+      }
+    }
+
+    const container = document.querySelector('#project-list');
+    emptyContainer();
+    projects.forEach((project) => {
+      const id = project.getID();
+      const title = project.getTitle();
+      const description = project.getDescr();
+      const projectEle = projectElement(id, title, description)
+      container.append(projectEle);
+    })
+  }
+
+  const _renderPageContents = () => {
+    _renderProjects();
+  }
+
   const _closeModal = () => {
     const clicked = document.querySelectorAll('.close-modal');
     clicked.forEach((x) => {
@@ -29,6 +78,8 @@ const displayController = () => {
       // clear form
       title.value = '';
       description.value = '';
+
+      _renderPageContents();
     });
   };
 
