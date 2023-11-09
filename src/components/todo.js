5780ad42b7801dde23fe293276c303d077def7f4
Lindelwa <lindelwa.codes@gmail.com>
2023-11-09 08:36:18 +0200

    Update the todo component

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,73 +1,101 @@
 import { format, isToday, isTomorrow } from 'date-fns';
 
 const todo = (todoInstance) => {
-  const container = document.createElement('div');
-  container.className = 'task';
-  container.dataset.id = todoInstance.getID();
-  container.title = todoInstance.getDescr();
+  const radio = {
+    options: { classList: ['radio', 'complete-toggle'] },
+    children: [{
+      options: { classList: ['disk', todoInstance.taskCompleted() ? 'active' : ''] }
+    }]
+  };
 
-  let priority;
-  switch (todoInstance.getPriority()) {
-    case 0:
-      priority = 'high-priority';
-      break;
+  const title = () => {
+    let priority;
+    switch (todoInstance.getPriority()) {
+      case 0:
+        priority = 'high-priority';
+        break;
 
-    case 1:
-      priority = 'medium-priority';
-      break;
+      case 1:
+        priority = 'medium-priority';
+        break;
 
-    case 2:
-      priority = 'low-priority';
-      break;
+      case 2:
+        priority = 'low-priority';
+        break;
+    }
+
+    return {
+      children: [
+        { 
+          tagName: 'span', 
+          text: todoInstance.getTitle(), 
+          options: { className: 'title' } 
+        },
+        { tagName: 'span', options: { className: priority } },
+      ]
+    }
   }
 
-  const completed = todoInstance.taskCompleted() ? 'active' : '';
-  const title = todoInstance.getTitle();
-
-  const dueDate = todoInstance.getDueDate();
-  let dateDisplay;
-  if (isToday(dueDate)) {
-    dateDisplay = 'Today';
-  } else if (isTomorrow(dueDate)) {
-    dateDisplay = 'Tomorrow';
-  } else {
-    dateDisplay = `on ${format(todoInstance.getDueDate(), 'PPPP')}`;
+  const interactions = {
+    children: [
+      { 
+        tagName: 'img', 
+        options: { src: pencilFill, alt: 'Icon', className: 'edit-todo' }
+      },
+      { 
+        tagName: 'img', 
+        options: { src: trash3, alt: 'Icon', className: 'delete-todo' }
+      }
+    ]
   }
 
-  container.innerHTML = `
-    <div class="radio complete-toggle">
-      <div class="disk ${completed}"></div>
-    </div>
-    <div>
-      <span class="title">${title}</span>
-      <span class="${priority}"></span>
-    </div>
-    <div class="pencil-container">
-      <i class="bi bi-pencil-fill edit-todo"></i>
-      <i class="bi bi-trash3 delete-todo"></i>
-    </div>
-    <div class="deadline-container">
-      <span>Due ${dateDisplay}</span>
-      <i class="bi bi-clock"></i>
-    </div>
-  `;
+  const deadline = () => {
+    const dueDate = todoInstance.getDueDate();
+    let dateDisplay;
+    if (isToday(dueDate)) {
+      dateDisplay = 'Today';
+    } else if (isTomorrow(dueDate)) {
+      dateDisplay = 'Tomorrow';
+    } else {
+      dateDisplay = `on ${format(todoInstance.getDueDate(), 'PPPP')}`;
+    }
 
-  if (todoInstance.getLabels()[0]) {
-    const labels = todoInstance.getLabels().map((label) => {
-      const div = document.createElement('div');
-      div.textContent = label;
-      return div;
-    });
-
-    const labelsContainer = document.createElement('div');
-    labelsContainer.className = 'labels';
-
-    labels.forEach((label) => labelsContainer.appendChild(label));
-
-    container.appendChild(labelsContainer);
+    return {
+      options: { className: 'deadline-container' },
+      children: [
+        { tagName: 'span', text: `Due on ${dateDisplay}` }, 
+        { tagName: 'img', options: { src: clock, alt: 'Icon' } }
+      ]
+    }
   }
 
-  return container;
-};
+  const labels = () => {
+    const label = (text) => ({ text });
+
+    const container = {
+      options: { className: 'labels' },
+      children: [],
+    };
+
+    if (todoInstance.getLabels().length > 0) {
+      todoInstance.getLabels().forEach((item) => {
+        container.children.push(label(item));
+      });
+    }
+
+    return container;
+  }
+
+  return {
+    before: (el) => {
+      el.dataset.id = todoInstance.getID();
+    },
+    options: {
+      className: 'task',
+      title: todoInstance.getDescr(),
+    },
+    children: [radio, title(), interactions, deadline(), labels()]
+  }
+}
 
 export default todo;
