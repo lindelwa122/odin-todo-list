5c35fba2501656e6b457d7fce39b79b0df57ea48
Lindelwa <lindelwa.codes@gmail.com>
2023-11-09 07:42:06 +0200

    Update project component

--- src/components/project.js
+++ src/components/project.js
@@ -1,23 +1,26 @@
-/**
- * Creates a project element to be displayed in a list
- * @param {InstanceType} projectInstance - Project
- * @return {HTMLDivElement} An HTMLDIVElement that contains the icon and project's name
- */
+import boxes from 'bootstrap-icons/icons/boxes.svg';
+
 const project = (projectInstance) => {
-  const container = document.createElement('div');
-  container.className = 'project-item';
-  container.dataset.id = projectInstance.getID();
-  container.title = projectInstance.getDescr();
+  const icon = {
+    tagName: 'img',
+    options: { src: boxes, alt: 'icon' },
+  }
 
-  const icon = document.createElement('i');
-  icon.classList = 'bi bi-boxes';
+  const text = {
+    tagName: 'span',
+    text: projectInstance.getTitle(),
+  };
 
-  const text = document.createElement('span');
-  text.textContent = projectInstance.getTitle();
-
-  container.append(icon, text);
-
-  return container;
-};
+  return {
+    before: (el) => {
+      el.dataset.id = projectInstance.getID();
+    },
+    options: {
+      className: 'project-item',
+      title: projectInstance.getDescr(),
+    },
+    children: [icon, text],
+  };
+}
 
 export default project;
