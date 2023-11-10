9cc1b0adfb9d7db798e9a56b194029038ba789ce
Lindelwa <lindelwa.codes@gmail.com>
2023-11-10 05:15:13 +0200

    Add the container of the main content

---
+++ src/components/main.js
@@ -0,0 +1,26 @@
+import plusLg from 'bootstrap-icons/icons/plus-lg.svg';
+
+const main = () => {
+  const heading = { options: { className: 'heading' } };
+  const tasks = { options: { className: 'tasks' } };
+  const newTasks = {
+    options: { className: 'new-task' },
+    children: [
+      {
+        tagName: 'img',
+        options: { src: plusLg, alt: 'Icon' }
+      },
+      {
+        tagName: 'span',
+        options: { textContent: 'Add task' }
+      }
+    ]
+  };
+
+  return {
+    tagName: 'main', 
+    children: [heading, tasks, newTasks]
+  };
+}
+
+export default main();
