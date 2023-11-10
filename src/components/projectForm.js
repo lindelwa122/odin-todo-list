6f23b9e63d4d208f924340659704f95d0cb2fb12
Lindelwa <lindelwa.codes@gmail.com>
2023-11-10 05:04:02 +0200

    Add a project form component

---
+++ src/components/projectForm.js
@@ -0,0 +1,53 @@
+const projectForm = () => {
+  const closeBtn = {
+    children: [{
+      tagName: 'img',
+      options: { 
+        className: 'close-modal', 
+        src: xLg, 
+        alt: 'Icon' 
+      }
+    }]
+  }
+
+  const form = () => {
+    const formField = (tag, options) => ({
+      tagName: tag,
+      options: options
+    });
+
+    return {
+      tagName: 'form',
+      options: { id: 'project-form', method: 'dialog' },
+      children: [
+        formField('label', { for: 'project-title', textContent: 'Project Title' }),
+        formField('input', { 
+          id: 'project-title',
+          name: 'project-title',
+          placeholder: 'What name do you have in mind?',
+          minLength: 3,
+          maxLength: 20,
+          required: true
+        }),
+        formField('label', { for: 'project-description', textContent: 'Description' }),
+        formField('textarea', { 
+          id: 'project-description',
+          name: 'project-description',
+          minLength: 10,
+          maxLength: 250,
+          cols: 30,
+          rows: 10
+        }),
+        { tagName: 'button', text: 'Add A New Project' }
+      ]
+    }
+  }
+
+  return {
+    tagName: 'dialog',
+    options: { id: 'project-form-dialog' },
+    children: [closeBtn, form()]
+  }
+}
+
+export default projectForm();
