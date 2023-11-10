2801fb42e22323cc85d38ec051d8285624fde76f
Lindelwa <lindelwa.codes@gmail.com>
2023-11-10 11:58:17 +0200

    Handle project form submit.

--- src/components/projectForm.js
+++ src/components/projectForm.js
@@ -1,4 +1,6 @@
 import xLg from 'bootstrap-icons/icons/x-lg.svg';
+import userInterfaceAPI from '../modules/userInterfaceAPI';
+import displayController from '../modules/displayController';
 
 const projectForm = () => {
   const show = () => {
@@ -25,6 +27,22 @@ const projectForm = () => {
     }
   
     const form = () => {
+      const getFormData = (form) => {
+        const formData = {};
+    
+        for (const field of form) {
+          formData[field.name] = field.value;
+        }
+    
+        return formData;
+      }
+    
+      const submitHandler = (e) => {
+        const { title, description } = getFormData(e.target);
+        userInterfaceAPI.createProject(title, description);
+        displayController.displayProjects();
+      }
+
       const formField = (tag, options) => ({
         tagName: tag,
         options: options
@@ -32,12 +50,16 @@ const projectForm = () => {
   
       return {
         tagName: 'form',
-        options: { id: 'project-form', method: 'dialog' },
+        options: { 
+          id: 'project-form', 
+          method: 'dialog',
+          onsubmit: submitHandler,
+        },
         children: [
           formField('label', { for: 'project-title', textContent: 'Project Title' }),
           formField('input', { 
             id: 'project-title',
-            name: 'project-title',
+            name: 'title',
             placeholder: 'What name do you have in mind?',
             minLength: 3,
             maxLength: 20,
@@ -46,7 +68,7 @@ const projectForm = () => {
           formField('label', { for: 'project-description', textContent: 'Description' }),
           formField('textarea', { 
             id: 'project-description',
-            name: 'project-description',
+            name: 'description',
             minLength: 10,
             maxLength: 250,
             cols: 30,
