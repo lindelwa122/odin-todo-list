9370003e32690ab53e3e55f5060d789c30118ffd
Lindelwa <lindelwa.codes@gmail.com>
2023-11-11 17:57:29 +0200

    Add routes

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,8 +1,11 @@
 import nav from "../components/nav";
 import sidebar from "../components/sidebar";
+import todoForm from "../components/todoForm";
+import projectForm from "../components/projectForm";
+import main from "../components/main";
 
 const root = {
-  children: [nav, sidebar]
+  children: [nav, sidebar, todoForm.content(), projectForm.content(), main]
 };
 
 export default root;
